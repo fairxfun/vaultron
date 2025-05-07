dcaf27531ce565b64764308d2930656a6ff59568
mikerfrr <mike@fairx.fun>
2025-05-07 15:06:33 +0800

    change coordinator service

--- integration_tester/src/main.rs
+++ integration_tester/src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use integration_tester::{
-    enclave::enclave_test, enclave_agent::enclave_agent_test, integration_trace_init,
-    service_discovery::service_discovery_test, EnclaveTesterError, MessageHandler,
+    enclave::enclave_test, enclave_agent::enclave_agent_test, integration_trace_init, EnclaveTesterError,
+    MessageHandler,
 };
 use log::info;
 use std::env;
@@ -29,19 +29,5 @@ async fn start_test(pcr0: Vec<u8>) -> Result<(), EnclaveTesterError> {
     let mut handler = MessageHandler::new(pcr0.clone()).await?;
     enclave_agent_test(&mut handler).await?;
     enclave_test(&mut handler, pcr0).await?;
-    service_discovery_test().await?;
     Ok(())
-
-    // let cluster_public_key = test_init_cluster_key(&mut handler).await;
-
-    // let cluster_attributes = ClusterAttributes::builder()
-    //     .enclave_pcr0(pcr0)
-    //     .cluster_public_key(cluster_public_key)
-    //     .build();
-    // let iteration_start = Instant::now();
-    // test_create_enclave_wallet(cluster_attributes, &mut handler).await;
-    // let iteration_end = Instant::now();
-    // let duration = iteration_end.duration_since(iteration_start);
-    // println!(" test create enclave wallet done in {:?}", duration);
-    // sleep(Duration::from_secs(3)).await;
 }
